e76a998fec89ee9b3beb4a71bd3bea7eda397e84
Gustavo <gustavo.jesus@tst.jus.br>
2020-06-02 21:27:44 -0300

    :truck: Create directives folder and place ripple-effect directive in it

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RippleEffectDirective } from './components/ripple-effect/ripple-effect.directive';
 import { SettingsButtonComponent } from './components/settings-button/settings-button.component';
+import { RippleEffectDirective } from './directives/ripple-effect/ripple-effect.directive';
 
 @NgModule({
    declarations: [
