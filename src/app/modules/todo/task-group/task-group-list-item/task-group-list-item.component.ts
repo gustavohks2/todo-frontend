c065309e4942246140e85faf34f8631f4d1368a7
Gustavo <gustavo.jesus@tst.jus.br>
2020-06-02 23:29:48 -0300

    :lipstick: Style todo task group list and its items

---
+++ src/app/modules/todo/task-group/task-group-list-item/task-group-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-task-group-list-item',
+  templateUrl: './task-group-list-item.component.html',
+  styleUrls: ['./task-group-list-item.component.scss']
+})
+export class TaskGroupListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -4,13 +4,15 @@ import { TaskGroupComponent } from './task-group/task-group.component';
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TodoHeaderComponent } from './todo-header/todo-header.component';
 import { TodolistComponent } from './todolist/todolist.component';
+import { TaskGroupListItemComponent } from './task-group/task-group-list-item/task-group-list-item.component';
 
 @NgModule({
    declarations: [
       TaskGroupComponent,
       TaskItemComponent,
       TodoHeaderComponent,
-      TodolistComponent
+      TodolistComponent,
+      TaskGroupListItemComponent
    ],
    imports: [
       CommonModule
